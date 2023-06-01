a9b4885d7352d45ff619d13ed66bc14b85ab613b
Chen-Lin Chuang <maplerice0704@gmail.com>
2023-06-01 15:17:31 +0800

    include SLAM and seq1 result

---
+++ SLAM/ICP.py
@@ -0,0 +1,61 @@
+import os, sys, argparse, csv, copy
+import numpy as np
+import open3d as o3d
+from glob import glob
+
+
+def ICP(source, target, threshold, init_pose, iteration=30):
+    # implement iterative closet point and return transformation matrix
+    reg_p2p = o3d.pipelines.registration.registration_icp(
+        source, target, threshold, init_pose,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(),
+        o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=iteration)
+    )
+    print(reg_p2p)
+    print(reg_p2p.transformation)
+    return reg_p2p.transformation
+
+def csv_reader(filename):
+    # read csv file into numpy array
+    data = np.loadtxt(filename, delimiter=',')
+    return data
+
+def numpy2pcd(arr):
+    # turn numpy array into open3d point cloud format
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(arr)
+    return pcd
+
+
+if __name__ == '__main__':
+
+    with open('../ITRI_dataset/seq1/localization_timestamp.txt', 'r') as file:
+        lines = file.readlines()
+
+	# Remove the newline character ('\n') from each line
+    lines = [line.strip() for line in lines]
+    with open('pred_pose.txt', 'w+') as pred:
+        for i in range(len(lines)):
+            path_name = '../ITRI_dataset/seq1/dataset/%s' % lines[i]
+            output_path = "./output/%s" % lines[i]
+            # Target point cloud
+            target = csv_reader(f'{path_name}/sub_map.csv')
+            target_pcd = numpy2pcd(target)
+
+            # Source point cloud
+            #TODO: Read your point cloud here#
+            source = csv_reader(f'{output_path}/output.csv')
+            source_pcd = numpy2pcd(source)
+
+            # Initial pose
+            init_pose = csv_reader(f'{path_name}/initial_pose.csv')
+
+            # Implement ICP
+            transformation = ICP(source_pcd, target_pcd, threshold=0.02, init_pose=init_pose)
+            pred_x = transformation[0,3]
+            pred_y = transformation[1,3]
+            print(pred_x, pred_y)
+            pred.write(str(pred_x))
+            pred.write(",")
+            pred.write(str(pred_y))
+            pred.write("\n")
---
+++ SLAM/README.md
@@ -0,0 +1,14 @@
+# SLAM
+#### Simultaneous localization and mapping
+
+![](output/slam_frame.png)
+### point cloud using open3d
+![](output/slam_open3d.png)
+### point cloud using matplotlib
+![](output/slam_plt.png)
+
+## Libraries used
+* [opencv](https://opencv.org/)
+* [numpy](https://numpy.org/)
+* [open3d](http://www.open3d.org/)
+* [matplotlib](https://matplotlib.org/)
