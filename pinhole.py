a426a42f2dddef69abc233191feaa25eebebd101
YellowJason <jason90814@gmail.com>
2023-06-06 17:04:51 +0800

    Fix pinhole problem, output base_link coordinate

--- pinhole.py
+++ pinhole.py
@@ -5,6 +5,17 @@ import os
 import yaml
 import csv
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation
+
+qs = {'b_2_fl': np.array([0.074732, -0.794, -0.10595, 0.59393]),
+      'fl_2_f': np.array([-0.117199, -0.575476, -0.0686302, 0.806462]),
+      'fr_2_f': np.array([-0.0806252, 0.607127, 0.0356452, 0.789699]),
+      'f_2_base': np.array([-0.5070558775462676, 0.47615311808704197, -0.4812773544166568, 0.5334272708696808])}
+
+rotation_b_fl = Rotation.from_quat(qs['b_2_fl'])
+rotation_fl_f = Rotation.from_quat(qs['fl_2_f'])
+rotation_fr_f = Rotation.from_quat(qs['fr_2_f'])
+rotation_f_base = Rotation.from_quat(qs['f_2_base'])
 
 def main():
     parser = argparse.ArgumentParser(description = 'Read dataset & marker')
@@ -32,7 +43,7 @@ def main():
     for i in range(len(lines)):
         line = lines[i] 
         print('\r', end='')
-        print(f'Processing {i+1}/{len(lines)}', end='')
+        print(f'Processing {i+1}/{len(lines)}', end=' ')
         floder_path = os.path.join(seq_path, 'dataset', line[:-1]) # remove '\n'
         # read image
         img = cv2.imread(os.path.join(floder_path, 'raw_image.jpg')).astype('uint8')
@@ -42,8 +53,8 @@ def main():
         camera = csv.reader(camera_f)
         for ca in camera:
             camera = str(ca[0])
-            camera = camera.split('_')[4] # fl, f, b, fr
-            camera = cameras[camera]
+            camera_name = camera.split('_')[4] # fl, f, b, fr
+            camera = cameras[camera_name]
         p_matrix = np.array(camera['projection_matrix']['data']).reshape((3,4))
         # print(p_matrix)
         # read corners
@@ -53,36 +64,62 @@ def main():
         x_row = corners[:, 1]
         y_row = corners[:, 0]
 
-        dsty = (1.63*p_matrix[1,1]) / (y_row-p_matrix[1,2])
-        dstx = (x_row-p_matrix[0,2]) * (dsty/p_matrix[0,0])
-        # save raw data
+        # Normalized 3D point (x, y, 1) at camera coordination
+        dstx = (x_row-p_matrix[0,2]) / p_matrix[0,0]
+        dsty = (y_row-p_matrix[1,2]) / p_matrix[1,1]
+        dstz = np.ones((len(dstx), 1))
+        
         dstx = dstx.reshape(-1,1)
         dsty = dsty.reshape(-1,1)
-        xy_origin = np.concatenate((dstx, dsty), axis=1)
-        file_path = os.path.join(floder_path, 'output_raw.csv')
-        np.savetxt(file_path, xy_origin, delimiter=',', fmt='%s')
+        dstz = dstz.reshape(-1,1)
+        xyz_in_camera = np.concatenate((dstx, dsty, dstz), axis=1)
+        # print(xyz_in_camera)
 
-        dstx_near = []
-        dsty_near = []
-        for i in range(len(dstx)):
-            if (dstx[i] < 100) and (dstx[i] > -100) and (dsty[i] < 100) and (dsty[i] > 0):
-                dstx_near.append(dstx[i])
-                dsty_near.append(dsty[i])
+        # Convert to base_link (only rotate), for base_link: x=front, y=left, z=top
+        # origin of base_link = origin of front camera
+        origin = np.array([0,0,0])
+        if camera_name == 'f':
+            xyz_in_base = rotation_f_base.apply(xyz_in_camera)
+        elif camera_name == 'fr':
+            xyz_in_base = rotation_fr_f.apply(xyz_in_camera)
+            origin = origin + np.array([0.559084, 0.0287952, -0.0950537])
+            xyz_in_base = rotation_f_base.apply(xyz_in_base)
+            origin = rotation_f_base.apply(origin)
+        elif camera_name == 'fl':
+            xyz_in_base = rotation_fl_f.apply(xyz_in_camera)
+            origin = origin + np.array([-0.564697, 0.0402756, -0.028059])
+            xyz_in_base = rotation_f_base.apply(xyz_in_base)
+            origin = rotation_f_base.apply(origin)
+        elif camera_name == 'b':
+            xyz_in_base = rotation_b_fl.apply(xyz_in_camera)
+            origin = origin + np.array([-1.2446, 0.21365, -0.91917])
+            xyz_in_base = rotation_fl_f.apply(xyz_in_base)
+            origin = rotation_fl_f.apply(origin) + np.array([-0.564697, 0.0402756, -0.028059])
+            xyz_in_base = rotation_f_base.apply(xyz_in_base)
+            origin = rotation_f_base.apply(origin)
+        # print(camera_name, origin)
+
+        # Find the cross point with ground
+        xyz_in_base = xyz_in_base * -1.63 / xyz_in_base[:, 2].reshape((-1,1))
+
+        # print(xyz_in_base)
+        near_point = []
+        min_d = 15
+        for i in range(len(xyz_in_base)):
+            dist = xyz_in_base[i,0]**2 + xyz_in_base[i,1]**2
+            if dist < min_d**2:
+                near_point.append(xyz_in_base[i])
+        near_point = np.array(near_point)
+        print('Point number:', near_point.shape, end = '')
+        if len(near_point) != 0:
+            plt.scatter(near_point[:,0], near_point[:,1], s=3)
+            plt.xlim([-min_d, min_d])
+            plt.ylim([-min_d, min_d])
+            plt.savefig(os.path.join(floder_path, 'plot.png'))
+            plt.close()
         
-        dstx = np.array(dstx_near).reshape(-1,1)
-        dsty = np.array(dsty_near).reshape(-1,1)
-        dstx = dstx.reshape(-1,1)
-        dsty = dsty.reshape(-1,1)
-        plt.scatter(dstx, dsty, s=3)
-        plt.savefig(os.path.join(floder_path, 'plot.png'))
-        plt.close()
-        
-        z = np.ones((len(dstx), 1)) * 1.63
-        # print(dstx.shape, dsty.shape, z.shape)
-        xyz = np.concatenate((dstx, dsty, z), axis=1)
-        # print(xyz.shape)
         file_path = os.path.join(floder_path, 'output.csv')
-        np.savetxt(file_path, xyz, delimiter=',', fmt='%s')
+        np.savetxt(file_path, near_point, delimiter=',', fmt='%s')
 
 if __name__ == '__main__':
     main()
---
+++ rotate.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import math
+from scipy.spatial.transform import Rotation
+
+qs = {'b_2_fl': np.array([0.074732, -0.794, -0.10595, 0.59393]),
+      'fl_2_r': np.array([-0.117199, -0.575476, -0.0686302, 0.806462]),
+      'fr_2_r': np.array([-0.0806252, 0.607127, 0.0356452, 0.789699]),
+      'f_2_base': np.array([-0.5070558775462676, 0.47615311808704197, -0.4812773544166568, 0.5334272708696808])}
+
+def quaternion_to_rotation_matrix(q):  # x, y ,z ,w
+    rot_matrix = np.array(
+        [[1.0 - 2 * (q[1] * q[1] + q[2] * q[2]), 2 * (q[0] * q[1] - q[3] * q[2]), 2 * (q[3] * q[1] + q[0] * q[2])],
+         [2 * (q[0] * q[1] + q[3] * q[2]), 1.0 - 2 * (q[0] * q[0] + q[2] * q[2]), 2 * (q[1] * q[2] - q[3] * q[0])],
+         [2 * (q[0] * q[2] - q[3] * q[1]), 2 * (q[1] * q[2] + q[3] * q[0]), 1.0 - 2 * (q[0] * q[0] + q[1] * q[1])]],
+        dtype=q.dtype)
+    return rot_matrix
+
+def quart_to_rpy(q):
+    [x, y, z, w] = q
+    roll = math.atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y))
+    pitch = math.asin(2 * (w * y - x * z))
+    yaw = math.atan2(2 * (w * z + x * y), 1 - 2 * (z * z + y * y))
+    return roll, pitch, yaw
+
+# 以x,y,z為軸的轉動
+(r, p, y) = quart_to_rpy(qs['fr_2_r'])
+print('fr', r,p,y)
+(r, p, y) = quart_to_rpy(qs['fl_2_r'])
+print('fl', r,p,y)
+(r, p, y) = quart_to_rpy(qs['b_2_fl'])
+print('b ', r,p,y)
+(r, p, y) = quart_to_rpy(qs['f_2_base'])
+print('base ', r,p,y)
+
+p1 = np.array([5, 0, 0]).T
+p2 = np.array([0, 0, 5]).T
+r_m = quaternion_to_rotation_matrix(qs['fr_2_r'])
+print(np.dot(r_m, p1), np.dot(r_m, p2))
+
+rotation = Rotation.from_quat(qs['f_2_base'])
+print(rotation.apply(p1), rotation.apply(p2))
